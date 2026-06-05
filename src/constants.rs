e70b55176a04f77d5f1d97a7ae62c8c36710260a
nope-e <273367430+nope-e@users.noreply.github.com>
2026-06-05 21:31:50 +0800

    Set default test pattern to 1080p60

--- src/constants.rs
+++ src/constants.rs
@@ -6,9 +6,9 @@ pub const ACTIVATE_CLSID_STRING: &str = "{44E66AC5-04BE-4E2F-9D15-FEA1430DF6B5}"
 pub const FRIENDLY_NAME: &str = "Rust StaticCam Prototype";
 pub const STREAM_ID: u32 = 0;
 
-pub const FRAME_WIDTH: u32 = 640;
-pub const FRAME_HEIGHT: u32 = 480;
-pub const FRAME_RATE_NUMERATOR: u32 = 30;
+pub const FRAME_WIDTH: u32 = 1920;
+pub const FRAME_HEIGHT: u32 = 1080;
+pub const FRAME_RATE_NUMERATOR: u32 = 60;
 pub const FRAME_RATE_DENOMINATOR: u32 = 1;
 pub const FRAME_DURATION_100NS: i64 = 10_000_000i64 / FRAME_RATE_NUMERATOR as i64;
 
